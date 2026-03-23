f88a9a24e2c3189b5891a8fae81f31c42928d176
trungdung1711 <trungdunglebui17112004@gmail.com>
2026-03-24 03:16:19 +0900

    feat: :sparkles: add conda export file and add simple exploratory data analysis

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,12 @@
 # 💀💀💀 Please install conda first before running this script
 # please install from https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html
 
-# create the virtual environment 
-conda create --name sentiment-analysis-on-movie-reviews python=3.12
+# ✅ Use this file to recreate the environment
+conda env create -f environment.yml
 
-# activate the environment
-conda activate sentiment-analysis-on-movie-reviews
+
+# ❌ create the virtual environment 
+# conda create --name sentiment-analysis-on-movie-reviews python=3.12
+
+# ❌ activate the environment
+# conda activate sentiment-analysis-on-movie-reviews
